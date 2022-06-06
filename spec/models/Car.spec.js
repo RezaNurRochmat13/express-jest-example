4f0160003a4fba6693d99d32e909003d19f5db78
Reja <rezanurrochmat@gmail.com>
2022-06-06 22:13:44 +0700

    add test model car

---
+++ spec/models/Car.spec.js
@@ -0,0 +1,19 @@
+const {
+    sequelize,
+    dataTypes,
+    checkModelName,
+    checkPropertyExists
+} = require('sequelize-test-helpers');
+
+const CarModel = require("../../app/models/car");
+
+describe('test properties on model', () => {
+    const Model = CarModel(sequelize, dataTypes);
+    const instance = new Model();
+
+    checkModelName(Model)('Car');
+
+    context('check properties', () => {
+        ['name', 'price', 'size', 'image', 'isCurrentlyRented'].forEach(checkPropertyExists(instance));
+    });
+});
