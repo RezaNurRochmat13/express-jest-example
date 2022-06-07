4ce0cf2ae47cde4fa0682e1cf901c3a14a500ada
Reja <rezanurrochmat@gmail.com>
2022-06-07 21:59:52 +0700

    Add test create car

--- spec/requests/Car.api.spec.js
+++ spec/requests/Car.api.spec.js
@@ -13,11 +13,75 @@ describe('test index api', () => {
     });
 });
 
-describe('test api cars', () => {
+describe('test api get all cars', () => {
     it('return 200 ok', (done) => {
         request(app)
         .get("/v1/cars")
         .expect('Content-Type', 'application/json; charset=utf-8')
         .expect(200, done);
+    });
+});
+
+describe('test api create car', () => {
+    it('return 201 created', async() => {
+        const loginAuth = {
+            email: 'hilmi@gmail.com',
+            password: 'hilmianakkampus'
+        };
+
+        await request(app)
+        .post("/v1/auth/register")
+        .send(loginAuth);
+
+        const response = await request(app)
+        .post("/v1/auth/login")
+        .send(loginAuth);
+
+        const token = `Bearer ${response.body.accessToken}`;
+
+        const carPayload = {
+            name: "Honda",
+            price: 1000,
+            size: "small"
+        };
+
+        await request(app)
+        .post("/v1/cars")
+        .set("Authorization", token)
+        .send(carPayload)
+        .expect(201)
+        .expect("Content-Type", "application/json; charset=utf-8");
+
+
+    });
+
+    it('return 401 unauthorized access', async() => {
+        const loginAuth = {
+            email: 'akbar@gmail.com',
+            password: 'akbaranakkorlap'
+        };
+
+        await request(app)
+        .post("/v1/auth/register")
+        .send(loginAuth);
+
+        const response = await request(app)
+        .post("/v1/auth/login")
+        .send(loginAuth);
+
+        const token = `Bearer ${response.body.accessToken}`;
+
+        const carPayload = {
+            name: "Honda",
+            price: 1000,
+            size: "small"
+        };
+
+        await request(app)
+        .post("/v1/cars")
+        .set("Authorization", token)
+        .send(carPayload)
+        .expect(401)
+        .expect("Content-Type", "application/json; charset=utf-8");
     });
 });
